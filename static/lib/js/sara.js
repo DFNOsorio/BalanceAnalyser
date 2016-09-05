8ffc2f19bb7f66fe5f74501da27b3d8ffe15f5bc
DanielOsorio <vdosavh@gmail.com>
2016-09-05 09:25:30 +0100

    Initial commit

---
+++ static/lib/js/sara.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+                      
+    $('#test').click(function(){
+        console.log("Hey")
+        $('#test').text("NOP")
+  });
+    
+    $(function() {
+    $('a#calculate').bind('click', function() {
+      $.getJSON($SCRIPT_ROOT + '/_add_numbers', {
+        a: $('input[name="a"]').val(),
+        b: $('input[name="b"]').val()
+      }, function(data) {
+        $("#result").text(data.result);
+      });
+      return false;
+    });
+  });
+    
+});
